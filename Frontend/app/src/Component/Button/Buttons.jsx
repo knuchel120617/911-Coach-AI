6c236ca8f0fb747aeeb1244a104c5c8695aa987e
Mahlet <mahitimulu@gmail.com>
2024-07-09 10:15:40 -0500

    added the button component

---
+++ Frontend/app/src/Component/Button/Buttons.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import classNames from "classnames";
+
+function Buttons({ children, primary, rounded, alternate, ...res }) {
+  const classes = classNames(
+    res.className,
+    " border cursor-pointer",
+    {
+      "border-[1px] border-black bg-[#009379] text-center w-[221px] h-[60px] text-black px-[50px] py-[16px] border-none  hover:bg-[#4cbfaa] tracking-[2px]":
+        primary,
+      "rounded-[20px]": rounded,
+    },
+
+    {
+      "border-[1px] border-black bg-[#fff] text-center w-[221px] h-[60px] text-black px-[50px] py-[16px] border-[#009379]  hover:bg-[#dadada] tracking-[2px]":
+        alternate,
+      "rounded-[20px]": rounded,
+    },
+  );
+
+  return (
+    <>
+      <button {...res} className={classes}>
+        {children}
+      </button>
+    </>
+  );
+}
+
+Buttons.propTypes = {
+  checkValues: ({ primary, alternate }) => {
+    const count = Number(!!primary) + Number(!!alternate);
+    if (count > 1) {
+      return new Error(
+        "Can not have primary, secondary , tenary buttons, choose one",
+      );
+    }
+  },
+};
+
+export default Buttons;
