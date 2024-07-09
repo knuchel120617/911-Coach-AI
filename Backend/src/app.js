0e6a6a738e8bc8ceac6b5b67fca33169b5f5b82b
Marika <marika.bergman@gmail.com>
2024-07-09 10:23:16 +0100

    setup database connection

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express"
-//import listEndpoints from "express-list-endpoints"
+import listEndpoints from "express-list-endpoints"
 import cors from "cors"
+import mongoose from "mongoose"
 
 
 // import routers
@@ -32,4 +33,13 @@ app.use(cors(corsOptions))
 
 // ---------------- DATABASE ------------------
 
-// setting up database connection
+mongoose.set("strictQuery", false)
+mongoose.connect(process.env.MONGO_URL)
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB")
+  app.listen(port, () => {
+    console.table(listEndpoints(app))
+    console.log(`Server running on port ${port}`)
+  })
+})
