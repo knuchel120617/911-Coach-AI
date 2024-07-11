913f3992b49b985633488b4aae47c18863a46c57
Mahlet <mahitimulu@gmail.com>
2024-07-10 19:43:28 -0500

    added the signup page

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import Buttons from "./Component/Button/Buttons";
 import HomePage from "./Page/HomePage";
+import SinupPage from "./Page/SingupPage";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <SinupPage />
     </>
   );
 }
---
+++ Frontend/app/src/Page/SingupPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Signup from "../Component/Singup/Singup";
+
+function SinupPage() {
+  return (
+    <main>
+      <Signup />
+    </main>
+  );
+}
+
+export default SinupPage;
