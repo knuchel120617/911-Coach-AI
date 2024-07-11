0f3319395f89478c836eda077e689e55ba6ee9fc
Mahlet <mahitimulu@gmail.com>
2024-07-11 11:54:43 -0500

    added the signin page

--- Frontend/app/src/App.jsx
+++ Frontend/app/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import Buttons from "./Component/Button/Buttons";
 import HomePage from "./Page/HomePage";
-import SinupPage from "./Page/SingupPage";
+import SignupPage from "./Page/SignupPage";
+import SigninPage from "./Page/SigninPage";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <HomePage />
+      <SigninPage />
     </>
   );
 }
