95a300b2bce2e2714c4088d16b98518651fed383
Alaa Dania Adimi <59611591+adimidania@users.noreply.github.com>
2024-07-13 02:49:23 +0100

    Add endpoints

--- AI/api/app/main.py
+++ AI/api/app/main.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI, HTTPException, BackgroundTasks
 from utils import *
 from pydantic import BaseModel
+from typing import List
 
 
 '''The API '''
 # Create the FastAPI app
 app = FastAPI()
 
-# Request Model
-class RequestModel(BaseModel):
-    message: str 
+# Models
+class ScenarioModel(BaseModel):
     emergency_type: str
+
+class QuestionModel(BaseModel):
+    question: str 
+
+class MessageModel(BaseModel):
+    author: str
+    comment: str
+
+class ConversationModel(BaseModel):
+    chat_history: List[MessageModel]
     scenario: str
+    conversation: str
 
 # Define the root route
 @app.get("/")
@@ -21,15 +32,35 @@ async def root():
     """
     return {"message": "Welcome to the smart dispatch API!"}
 
-# Question and Answer endpoint
+# Question & Answer Endpoint
 @app.post("/qa")
-async def qa(request: RequestModel):
-    message = request.message
+async def qa(request: QuestionModel):
+    message = request.question
     response = question_answer(message)
     return {"response": response}
 
-# Scenario Endpoint
+# Get scenario Endpoint
+@app.post("/scenario")
+async def scenario(request: ScenarioModel):
+    emergency_type = request.emergency_type
+    response = get_scenario(emergency_type)
+    return response
 
-@app.post("/ingest")
-async def qa(request: RequestModel):
-    pass
+# Simulation Endpoint
+@app.post("/simulation")
+async def simulate(request: ConversationModel):
+    chat_history = request.chat_history
+    scenario = request.scenario
+    conversation = request.conversation
+    print(chat_history, scenario, conversation)
+    response = simulate_chat(chat_history, scenario, conversation)
+    return {"AI response": response}
+
+# Feedback Endpoint
+@app.post("/feedback")
+async def scenario(request: ScenarioModel):
+    chat_history = request.emergency_type
+    scenario = request.scenario
+    conversation = request.conversation
+    response = get_scenario(chat_history, scenario, conversation)
+    return response
