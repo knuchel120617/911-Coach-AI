71700e8c714c90b31842b252dc7bba5651ceb6aa
Marika <marika.bergman@gmail.com>
2024-07-13 22:58:08 +0100

    update content

--- Frontend/app/src/Component/Home/Hero.jsx
+++ Frontend/app/src/Component/Home/Hero.jsx
@@ -14,11 +14,10 @@ function Hero() {
         <div className="mb-[30px] text-center flex flex-col items-center justify-center gap-8">
           <div className="text-center">
             <h1 className="text-4xl font-bold mb-4 lg:text-5xl" style={{ color: '#333333' }}>
-              Meet EM-Buddy: your free AI-powered EMD assistant
+              Meet 999 Coach AI: your free AI-powered EMD assistant
             </h1>
             <p className="text-xl text-center leading-relaxed text-gray-700" style={{ color: '#333333' }}>
-              Spend less time on prep and more time with your students. Khanmigo
-              is now completely, 100% free.
+              Simulate the most common medical emergency scenarios to make sure you are following the correct protocols. 
             </p>
           </div>
           <div className="w-auto">
